4c173bfc8a0350dd7aeb0207c1d257c12f767862
Marc N. <git@marcnitzsche.de>
2025-01-24 12:52:14 +0100

    Refactor Card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,8 @@
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import styles from "./index.module.css";
 import Card from "../components/Card";
 
 function Home() {
-  const context = useDocusaurusContext();
-  const { siteConfig = {} } = context;
-
   return (
     <Layout title="Homepage" description="EigenDA Documentation">
       <main className={styles.main}>
@@ -19,67 +15,38 @@ function Home() {
             <div className="row cards__container">
               <Card
                 to="overview"
-                header={{
-                  label: "About EigenDA",
-                }}
-                body={{
-                  label:
-                    "Start your journey with an overview of the EigenDA protocol",
-                }}
+                header="About EigenDA"
+                body="Start your journey with an overview of the EigenDA protocol"
               />
 
               <Card
                 to="integrations-guides/dispersal/quick-start"
-                header={{
-                  label: "Quick Start",
-                }}
-                body={{
-                  label:
-                    "Learn how EigenDA works by dispersing and retrieving a blob on the command line",
-                }}
+                header="Quick Start"
+                body="Learn how EigenDA works by dispersing and retrieving a blob on the command line"
               />
 
               <Card
                 to="operator-guides/overview"
-                header={{
-                  label: "Operator Guides",
-                }}
-                body={{
-                  label: "Look here for docs on spinning up an EigenDA node",
-                }}
+                header="Operator Guides"
+                body="Look here for docs on spinning up an EigenDA node"
               />
 
               <Card
                 to="integrations-guides/rollup-guides/integrations-overview"
-                header={{
-                  label: "Integrations Guides",
-                }}
-                body={{
-                  label:
-                    "Ready to launch a rollup on EigenDA? Get started here",
-                }}
+                header="Integrations Guides"
+                body="Ready to launch a rollup on EigenDA? Get started here"
               />
 
               <Card
                 to="api/v1/disperser/overview"
-                header={{
-                  label: "API Reference",
-                }}
-                body={{
-                  label:
-                    "These pages document the details of how to integrate with EigenDA",
-                }}
+                header="API Reference"
+                body="These pages document the details of how to integrate with EigenDA"
               />
 
               <Card
                 to="https://forms.gle/CMSJTxQPXfG5HTQQ7"
-                header={{
-                  label: "Register for EigenDA Mainnet",
-                }}
-                body={{
-                  label:
-                    "EigenDA Mainnet will soon be permissionless; until then please register here to launch your rollup",
-                }}
+                header="Register for EigenDA Mainnet"
+                body="EigenDA Mainnet will soon be permissionless; until then please register here to launch your rollup"
               />
             </div>
           </div>
