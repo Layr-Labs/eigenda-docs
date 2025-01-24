1277ad9e9c62e4b6263e5d0f25c0301003f392f0
Marc N. <git@marcnitzsche.de>
2025-01-24 12:28:06 +0100

    Update card styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,7 +14,7 @@ function Card({ to, header, body, externalIcon = false }) {
 
   return (
     <div className={clsx("col col--4 ", styles.feature)}>
-      <Link className="navbar__link card" to={to}>
+      <Link className="card" to={to}>
         <div className="card__header">
           <h2>
             {header.label}
@@ -35,7 +35,7 @@ function Card({ to, header, body, externalIcon = false }) {
           </h2>
         </div>
         <div className="card__body">
-          <p>{body.label}</p>
+          <p>{body.label} →</p>
         </div>
       </Link>
     </div>
